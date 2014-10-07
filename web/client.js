9a5a2fec4a9991a6cdd0b8ccdf42629551e00883
Antonio Trapani <antonio.trapani@gmail.com>
2014-10-07 13:53:14 +0200

    prod host

--- web/client.js
+++ web/client.js
@@ -1,5 +1,5 @@
 $(function() {
-    var HOST = 'http://margo.fubles.com:8080',
+    var HOST = 'http://margot.fubles.com:8080',
         STORAGE_USER_KEY = 'margot.user';
 
     var AppViewModel = function() {
