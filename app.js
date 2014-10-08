4b3292e8a24fabe5fd7728c457d0bb116f6d1836
Antonio Trapani <antonio.trapani@gmail.com>
2014-10-08 10:16:14 +0200

    CORS allowed

--- app.js
+++ app.js
@@ -16,5 +16,24 @@ app.use('/api', require('./app/routes/dishes'));
 app.use('/api', require('./app/routes/weeks'));
 app.use('/api', require('./app/routes/tickets'));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://margot.fubles.com:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.listen(8080);
 console.log('Margot is cooking on port 8080');
